316b2c6a464b0bae7f270ecbebf71a766f05fa73
ignacioToledoTorres <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-04-23 23:40:58 -0400

    Dockering the backend ready

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,8 @@
 import express, { Application } from "express";
+import { hola } from "./test";
 
 const app: Application = express();
 
 app.listen(3000, () => {
-  console.log("App iniciada");
+  console.log("Servidor corriendo en el puerto 3000");
 });
