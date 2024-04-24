9cc8b275bde95c69ec1750741daaeb0742afc1cc
ignacioToledoTorres <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-04-23 23:59:30 -0400

    update project variables of enviroment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,10 @@
 import express, { Application } from "express";
+import "dotenv/config";
+
+const PORT = process.env.PORT;
 
 const app: Application = express();
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en el puerto 3000");
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
