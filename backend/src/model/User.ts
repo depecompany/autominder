6d54c7e3775c6b4134070a0265d44a44d0d97e3a
ignacioToledoTorres <155652094+ignacioToledoTorres@users.noreply.github.com>
2024-04-29 17:31:33 -0400

    feat: register ready

---
+++ backend/src/model/User.ts
@@ -0,0 +1,100 @@
+import { QueryResult } from "pg";
+import dbConnection from "../config/db.config";
+import { AuthUser, UserInterface } from "../interfaces/User.inteface";
+
+const pool = dbConnection.pool;
+
+class User {
+  constructor() {}
+
+  async createTableUser(): Promise<void> {
+    const query = `
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR(255) NOT NULL,
+            username VARCHAR(100) NOT NULL,
+            firstName VARCHAR(100),
+            lastName VARCHAR(100),
+            password VARCHAR(150) NOT NULL,
+            carGroup JSONB,
+            isTester BOOLEAN NOT NULL,
+            isActive BOOLEAN NOT NULL,
+            role VARCHAR(50) NOT NULL,
+            hideProfile BOOLEAN,
+            avatar VARCHAR(255) NOT NULL,
+            allowPersonalDocuments BOOLEAN NOT NULL,
+            timezone VARCHAR(50) NOT NULL,
+            isPremium BOOLEAN NOT NULL
+        )
+    `;
+
+    try {
+      await pool.query(query);
+      console.log("Tabla usuarios creada correctamente.");
+    } catch (error) {
+      console.error("Error al crear la tabla usuarios:", error);
+      throw error;
+    }
+  }
+
+  public async saveUser(userData: UserInterface): Promise<void> {
+    const {
+      email,
+      username,
+      firstName,
+      lastName,
+      password,
+      carGroup,
+      isTester,
+      isActive,
+      role,
+      hideProfile,
+      avatar,
+      allowPersonalDocuments,
+      timezone,
+      isPremium,
+    } = userData;
+
+    const query = `
+        INSERT INTO users (email, username, firstName, lastName, password, carGroup, isTester, isActive, role, hideProfile, avatar, allowPersonalDocuments, timezone, isPremium)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    `;
+
+    const values = [
+      email,
+      username,
+      firstName,
+      lastName,
+      password,
+      carGroup,
+      isTester,
+      isActive,
+      role,
+      hideProfile,
+      avatar,
+      allowPersonalDocuments,
+      timezone,
+      isPremium,
+    ];
+
+    try {
+      await pool.query(query, values);
+      console.log("Usuario guardado correctamente.");
+    } catch (error) {
+      console.error("Error al guardar el usuario:", error);
+      throw error;
+    }
+  }
+
+  public async findByEmail(email: any): Promise<void> {
+    const query = "SELECT email FROM users WHERE email = $1";
+
+    try {
+      await pool.query(query, email);
+    } catch (err) {
+      console.log('Error during the function "findByEmail"');
+    }
+  }
+}
+
+export { User };
--- backend/src/route/ApiV1/index.ts
+++ backend/src/route/ApiV1/index.ts
@@ -1,23 +1,25 @@
-import { Router } from 'express';
-import { readdirSync } from 'fs';
+import { Router } from "express";
+import { readdirSync } from "fs";
 
 const PATH_ROUTER = `${__dirname}`;
 
 const router = Router();
 
 const cleanFileName = (fileName: string): string | undefined => {
-    const file = fileName.split('.').shift();
-    return file;
-}
+  const file = fileName.split(".").shift();
+  return file;
+};
 
 readdirSync(PATH_ROUTER).filter((fileName) => {
-    const cleanName = cleanFileName(fileName);
-    if (cleanName !== 'index') {
-        import(`./${cleanName}`).then((module) => {
-            console.log(`Se esta cargando la rutas http://localhost:4201/api/${cleanName}`);
-            router.use(`/api/${cleanName}`, module.router);
-        })
-    };
+  const cleanName = cleanFileName(fileName);
+  if (cleanName !== "index") {
+    import(`./${cleanName}`).then((module) => {
+      console.log(
+        `Se esta cargando la rutas API http://localhost:4201/api/${cleanName}`
+      );
+      router.use(`/${cleanName}`, module.router);
+    });
+  }
 });
 
-export { router }
+export { router };
