c31b5bb6c0ef7a547c5b942bdba0a1bba4e3c1b8
ignacioToledoTorres <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-05-04 00:07:52 -0400

    improve response login endpoint

--- backend/src/controller/APIv1/UserController.ts
+++ backend/src/controller/APIv1/UserController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import UserModel from "../../model/User";
 import { checkIfEmailExists } from "../../service/authService";
-import { encrypt } from "../../utils/handlerPass";
+import { encrypt, verified } from "../../utils/handlerPass";
 import { completeUserAuth } from "../../types/types";
 import { UserInterface } from "../../interfaces/User.inteface";
+import { generateToken } from "../../utils/handlerJWT";
 
 /**
  * @swagger
@@ -109,12 +110,41 @@ const registerNewUser = async ({ body }: Request, res: Response) => {
 };
 
 const login = async ({ body }: Request, res: Response) => {
+  let response = {
+    result: false,
+    action: "Error",
+    message: "Bad Request",
+    user: {},
+    status: 400,
+  };
   const { email, password } = body;
+  const emailAvaleible = await checkIfEmailExists(email);
 
-  let isAvaleibleEmail = true;
-  checkIfEmailExists(email).then((exists) => {
-    isAvaleibleEmail = false;
-  });
+  if (emailAvaleible) {
+    const user: any = await UserModel.findOne({ where: { email: email } })
+      .then((foundUser) => {
+        return foundUser?.dataValues;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    const verify = verified(password, user.email);
+    if (!verify) res.json(response).status(response.status);
+
+    console.log(user);
+    const token = generateToken(user);
+    delete user.password;
+    response.result = true;
+    response.action = "Success";
+    response.message = token;
+    response.status = 201;
+    response.user = user;
+
+    res.json(response).status(response.status);
+  } else {
+    res.json(response).status(response.status);
+  }
 };
 
-export { registerNewUser };
+export { registerNewUser, login };
--- backend/src/route/ApiV1/users.ts
+++ backend/src/route/ApiV1/users.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { registerNewUser } from "../../controller/APIv1/UserController";
+import { login, registerNewUser } from "../../controller/APIv1/UserController";
 
 const router = Router();
 
@@ -7,5 +7,7 @@ router.get("/auth", (req: Request, res: Response) => {
   res.json({ message: "hello world from API" });
 });
 router.post("/auth/register", registerNewUser);
+
+router.post("/auth/login", login);
 
 export { router };
