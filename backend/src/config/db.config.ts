68f5f858868471da2ab019334dcffdcf81ca6123
ignacioToledoTorres <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-05-11 01:25:05 -0400

    refacor db config

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,6 +1,5 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-import { Pool } from "pg";
 
 dotenv.config();
 
@@ -8,14 +7,6 @@ const POSTGRES_DATABASE = process.env.DB_DATABASE as string;
 const POSTGRES_USERNAME = process.env.DB_USERNAME as string;
 const POSTGRES_PASSWORD = process.env.DB_PASSWORD as string;
 const POSTGRES_PORT = process.env.DB_PORT as string;
-
-const pool = new Pool({
-  user: POSTGRES_USERNAME,
-  host: "postgres",
-  database: POSTGRES_DATABASE,
-  password: POSTGRES_PASSWORD,
-  port: parseInt(POSTGRES_PORT),
-});
 
 const sequelize = new Sequelize(
   POSTGRES_DATABASE,
@@ -30,6 +21,5 @@ const sequelize = new Sequelize(
 );
 
 export default {
-  pool: pool,
   sq: sequelize,
 };
