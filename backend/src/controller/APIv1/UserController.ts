4b5d39f64e56df96898502662a7a0f28458327b8
ignacioToledoTorres <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-05-10 23:11:49 -0400

    restore branch

--- backend/src/controller/APIv1/UserController.ts
+++ backend/src/controller/APIv1/UserController.ts
@@ -105,7 +105,6 @@ const registerNewUser = async ({ body }: Request, res: Response) => {
   } else {
     response.message = "conflict";
     response.status = 403;
-    response.message = "hola";
   }
 
   res.send(response).status(response.status);
