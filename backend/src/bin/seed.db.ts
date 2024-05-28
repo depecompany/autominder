c53878471262af46a0701bb1248f80b9606800dc
ignacioToledoTorres <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-05-28 00:22:49 -0400

    feat - database with data when build the app

--- backend/src/bin/seed.db.ts
+++ backend/src/bin/seed.db.ts
@@ -1,42 +1,42 @@
-import dbConfig from "../config/db.config";
-import CarModel from "../model/Car";
-import { encrypt } from "../utils/handlerPass";
-import UserModel from "../model/User";
+// import dbConfig from "../config/db.config";
+// import CarModel from "../model/Car";
+// import { encrypt } from "../utils/handlerPass";
+// import UserModel from "../model/User";
 
-const syncDatabase = async () => {
-  await dbConfig.sq.sync({ force: true });
+// const syncDatabase = async () => {
+//   await dbConfig.sq.sync({ force: true });
 
-  const predefinedData = {
-    isTester: false,
-    isActive: true,
-    role: "USER",
-    avatar: "avatar.jpg",
-    allowPersonalDocuments: false,
-    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-    isPremium: false,
-  };
-  const user = await UserModel.create({
-    username: "admin example",
-    email: "admin@example.com",
-    password: await encrypt("admin"),
-    ...predefinedData,
-  }).then((data) => {
-    return data.dataValues;
-  });
-  const car = await CarModel.create({
-    model: "Model X",
-    releaseYear: 2021,
-    brand: "Tesla",
-    isPersonal: true,
-    mileage: 10000,
-    chasis: 12345,
-    fuelType: "Electric",
-    userId: user.id,
-  });
+//   const predefinedData = {
+//     isTester: false,
+//     isActive: true,
+//     role: "USER",
+//     avatar: "avatar.jpg",
+//     allowPersonalDocuments: false,
+//     timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+//     isPremium: false,
+//   };
+//   const user = await UserModel.create({
+//     username: "admin example",
+//     email: "admin@example.com",
+//     password: await encrypt("admin"),
+//     ...predefinedData,
+//   }).then((data) => {
+//     return data.dataValues;
+//   });
+//   const car = await CarModel.create({
+//     model: "Model X",
+//     releaseYear: 2021,
+//     brand: "Tesla",
+//     isPersonal: true,
+//     mileage: 10000,
+//     chasis: 12345,
+//     fuelType: "Electric",
+//     userId: user.id,
+//   });
 
-  console.log("Base de datos poblada con datos iniciales");
-};
+//   console.log("Base de datos poblada con datos iniciales");
+// };
 
-syncDatabase().catch((error) =>
-  console.error("Error al sincronizar la base de datos:", error)
-);
+// syncDatabase().catch((error) =>
+//   console.error("Error al sincronizar la base de datos:", error)
+// );
