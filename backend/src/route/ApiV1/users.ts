4797e684668ae973679a5961c2fdb68990d969f4
Ignacio Toledo <95835776+IgnacioToledoDev@users.noreply.github.com>
2024-06-08 21:19:00 -0400

    feat - add login validate

--- backend/src/route/ApiV1/users.ts
+++ backend/src/route/ApiV1/users.ts
@@ -6,7 +6,7 @@ import {
   getUserById,
 } from "../../controller/APIv1/UserController";
 import { checkJwt } from "../../middleware/session";
-import { registerValidator } from "../../validators/AuthValidator";
+import { loginValidator, registerValidator } from "../../validators/AuthValidator";
 import validate from "../../middleware/validator";
 
 const router = Router();
@@ -17,7 +17,7 @@ router.get("/auth", (req: Request, res: Response) => {
 
 router.post("/auth/register", validate(registerValidator()), registerNewUser);
 
-router.post("/auth/login", login);
+router.post("/auth/login", validate(loginValidator()), login);
 
 router.post("/auth/send-email", sendEmailController);
 
